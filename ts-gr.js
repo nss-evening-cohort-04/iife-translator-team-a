e718c491d660b74b51f45373700bf4a5b2145bcd
Taylor Shuff <tkylesh@gmail.com>
2016-08-27 12:59:38 -0500

    added completed ts-gr.js file

--- ts-gr.js
+++ ts-gr.js
@@ -5,28 +5,46 @@ var translator = (function(german){
 	christmas:"weihnactea",
 	and:"und",
 	happy:"glucklich",
-	holidays:"ferien"};
+	new:"neu",
+	year: "Jahr"};
 
-
-german.getValue = function(key){
-	return translator.key;
-
-}
-
-var translatedStr = '';
-var str = document.getElementById('message').value;
-console.log(str);
-
-var arr = str.split(' ');
-console.log(arr);
-
-for(var i=0; i<str.length;i++){
-	translatedStr += translator.getValue(arr[i]);
 	
-}
 
-document.getElementById('translated').innerHTML= translatedStr;
 
+	german.printValue = function(){
+		var str = document.getElementById('message').value;
+		console.log(str);
+		
+		var arr = str.split(' ');
+		console.log(arr);
+	},
+
+	german.getValue = function(key){
+		return germanTxt[key];
+	},
+	german.outputValue = function(){
+		var translatedStr = '';
+
+		var str = document.getElementById('message').value;
+		var arr = str.split(' ');
+		console.log("entered the outputValue method");
+
+		for(var i=0; i<str.length; i++){
+			// translatedStr += translator.getValue(arr[i]);
+			arr[i] = translator.getValue(arr[i]);
+		}
+
+		document.getElementById('translated').innerHTML = arr.join(' ');
+	}
 
 return german;
 })(translator || {});
+
+////*****************************************************************************************////
+////////////Code listener for btn click then calls method within anonymous function////////////
+
+document.getElementById('german').addEventListener("click", function(){translator.outputValue()});
+document.getElementById('german').addEventListener("click", function(){translator.printValue()});
+
+
+
